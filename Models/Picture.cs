dbeeadae56e84b61b3fa27568577386861afc83e
pablito-1 <delaneyty@gmail.com>
2023-10-27 14:19:36 -0500

    added picture table

---
+++ Models/Picture.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IllinoisProject.Models
+{
+    public class Picture
+    {
+        public int PictureId { get; set; }
+        [Required]
+        public string AltAttribute { get; set; }
+        [NotMapped]
+        public IFormFile MyPicture { get; set; }
+        [DataType(DataType.Url)]
+        public string Url { get; set; }
+    }
+}
