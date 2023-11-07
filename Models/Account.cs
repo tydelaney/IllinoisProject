edbfdac017091dffea03260c51883247f74e1d7f
pablito-1 <delaneyty@gmail.com>
2023-11-07 17:23:38 -0600

    Confirm email no validation

--- Models/Account.cs
+++ Models/Account.cs
@@ -12,9 +12,9 @@ namespace IllinoisProject.Models
     public class Account
     {
         public int AccountId { get; set; }
+        public string AccountEmail { get; set; }
         public string AccountName { get; set; }
         public string UserName { get; set; }
-      
         public ICollection<BlogPost>? BlogPosts { get; set; }
     }
     
