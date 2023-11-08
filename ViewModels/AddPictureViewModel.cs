3633d4293a2a68c3d723524f469aa3fb956e3cc2
Colby4550 <colbyrinehart@gmail.com>
2023-11-07 21:33:13 -0600

    worked on edit and piccture

---
+++ ViewModels/AddPictureViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace IllinoisProject.ViewModels
+{
+    public class AddPictureViewModel
+    {
+        [Required(ErrorMessage = "Alt attribute is required.")]
+        [Display(Name = "Alt Attribute")]
+        public string AltAttribute { get; set; }
+
+        [Display(Name = "Select a file to upload")]
+        public IFormFile MyPicture { get; set; }
+    }
+}
