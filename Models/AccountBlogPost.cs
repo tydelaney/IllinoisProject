53dee1fdff7d80def5e30935dc5841178b79000e
pablito-1 <delaneyty@gmail.com>
2023-11-29 16:05:43 -0600

    added accountblogpost

---
+++ Models/AccountBlogPost.cs
@@ -0,0 +1,11 @@
+﻿namespace IllinoisProject.Models
+{
+    public class AccountBlogPost
+    {
+        public string Id { get; set; }
+        public string AccountId { get; set; }
+        public string BlogPostId { get; set; }
+        public Account Account { get; set; }
+        public BlogPost BlogPost { get; set;}
+    }
+}
